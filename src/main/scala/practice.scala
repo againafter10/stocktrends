a25337a7762f7d9508edbc15420c4be3befa3ceb
againafter10 <archana.joshi@gmail.com>
2018-10-21 14:09:10 +1100

    Update practice.scala

--- src/main/scala/practice.scala
+++ src/main/scala/practice.scala
@@ -1,12 +1,9 @@
 
 import org.apache.spark.sql.functions._
-import dataframe.dataframe
-import config.config
 import org.apache.log4j.{Level, Logger}
 import org.apache.spark.sql.{SparkSession, functions , DataFrame}
-import org.apache.spark.sql.functions.{avg, when, to_date }
+import org.apache.spark.sql.functions.{avg, when, to_date ,lag}
 import org.apache.spark.sql.expressions.Window
-import org.apache.spark.sql.functions.lag
 
 
 
@@ -17,16 +14,11 @@ object practice {
 
     Logger.getLogger("org").setLevel(Level.ERROR)
 
-    //var dataobj = new config()
-    //var stockobj = new dataframe()
-
     val cwd = System.getProperty("user.dir")
 
     val path = cwd + "/data/week20180810/*"
     println(path)
 
-    //stockobj.readCsv(dataobj.dataPath()).show()
-    //val stocks = stockobj.readCsv(dataobj.dataPath())
     val session = SparkSession.builder().appName("practice")
       .master("local[4]")
       .getOrCreate()
@@ -52,6 +44,8 @@ object practice {
     val trends = data_typed.withColumn("moving_average", avg(data_typed("adjcloseprice")).over(frame_4))
     trends.printSchema()
     trends.show()
+    
+    // joied dataset will have duplicate columns so select only needed ones
    var joined = trends.join(data_typed, data_typed("symbol") === trends("symbol"))
        .select(trends("symbol"),trends("moving_average"),trends("adjcloseprice"))
 
@@ -63,15 +57,12 @@ object practice {
               .when(joined.col("price_diff").>(0), "UP")
               .otherwise("DOWN")
 
-
-
     joined = joined.withColumn("trend", anlze)
     joined.show()
     val buy = when(joined.col("trend") === "DOWN" ,"CHEAP TODAY")
       .when(joined.col("trend") === "UP","Expensive Today")
       .otherwise("Same Price")
     joined.withColumn("infaltion",buy).show()
-
 
   }
 }
